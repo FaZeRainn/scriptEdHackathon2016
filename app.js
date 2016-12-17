02830b3b345315814a2a679a49c9403186009404
Daniel Suarez <dlsuarez.627@gmail.com>
2016-12-17 21:46:14 +0000

    Polar bear game

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
+$(document).ready(function() {
+   var x = 1300;
+   
+   function moveRight() {
+     x = x + 20;
+     $("#bear").css("left", x);
+   }
+  
 
-http://www.technocrazed.com/wp-content/uploads/2015/12/Earth-Wallpaper-112.jpg// JavaScript File
+  function moveLeft() { 
+    x = x - 20; 
+    if (x < 20) { 
+      $("#garbage").css("display", "none");
+    }
+    $('#bear').css('left', x);
+   } 
+
+    $(document).keydown(function(key) {
+        switch(parseInt(key.which,10)) {
+			// Left arrow key pressed
+            case 37:
+				moveLeft(); 
+				break;
+			// Up Arrow Pressed
+			case 38:
+				//$('#bear').animate({up: "-=10px"}, 'fast');
+				break;
+			// Right Arrow Pressed
+			case 39:
+				moveRight();
+				break;
+			// Down Arrow Pressed
+			case 40:
+				//$('#bear').animate({down: "+=10px"}, 'fast');
+				break;
+		}
+	});
+   
+ });
+
+
